0e6e1d944c9cbcd6cb982086498c7e036a169c7a
Antoine Dupuis <antoine.dupuis42@gmail.com>
2023-01-15 21:05:12 +0100

    Networking base setup done

---
+++ Multi-Test/Assets/PlayerNetwork.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    private Library library;
+    private GameObject projectilePrefab;
+
+    void Start() {
+        library = GameObject.Find("Library").GetComponent<Library>();
+        projectilePrefab = library.GetPlayerProjectilePrefab();
+    }
+    
+    // Update is called once per frame
+    void Update() {
+        if (!IsOwner) return;
+        
+        Vector3 moveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.Z)) moveDir.y = 1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.y = -1f;
+        if (Input.GetKey(KeyCode.Q)) moveDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = 1f;
+
+        float moveSpeed = 3f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        //Shoot
+        if (Input.GetKeyDown(KeyCode.Space)) Shoot();
+    }
+
+    void Shoot() {
+        Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+    }
+}
